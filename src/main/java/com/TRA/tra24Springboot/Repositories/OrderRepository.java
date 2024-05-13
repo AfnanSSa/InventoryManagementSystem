e2d06a033a335d29c1cffe18820f90d9c94135e2
AfnanSSa <iafnanz67@gmail.com>
2024-05-13 13:15:59 +0400

    Created Product Details Repository

--- src/main/java/com/TRA/tra24Springboot/Repositories/OrderRepository.java
+++ src/main/java/com/TRA/tra24Springboot/Repositories/OrderRepository.java
@@ -1,2 +1,7 @@
-package com.TRA.tra24Springboot.Repositories;public interface OrderRepository {
+package com.TRA.tra24Springboot.Repositories;
+
+import com.TRA.tra24Springboot.Models.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository <Order, Integer> {
 }
