aa3fc1b428d956b3a885c33cd8fd8949f70ff1ce
AfnanSSa <iafnanz67@gmail.com>
2024-05-13 13:14:01 +0400

    Created Supplier Repository

--- src/main/java/com/TRA/tra24Springboot/Repositories/SupplierRepository.java
+++ src/main/java/com/TRA/tra24Springboot/Repositories/SupplierRepository.java
@@ -2,6 +2,9 @@ package com.TRA.tra24Springboot.Repositories;
 
 import com.TRA.tra24Springboot.Models.Supplier;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.RequestParam;
 
-public interface SupplierRepositories extends JpaRepository <Supplier, Integer> {
+public interface SupplierRepository extends JpaRepository <Supplier, Integer> {
 }
