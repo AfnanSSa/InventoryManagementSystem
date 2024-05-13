55973e291b24e7dac1aaa1a6936caf01e001690f
AfnanSSa <iafnanz67@gmail.com>
2024-05-13 13:17:22 +0400

    Created Supplier Services Class

--- src/main/java/com/TRA/tra24Springboot/Services/SupplierServices.java
+++ src/main/java/com/TRA/tra24Springboot/Services/SupplierServices.java
@@ -1,2 +1,95 @@
-package com.TRA.tra24Springboot.Services;public class SupplierServices {
+package com.TRA.tra24Springboot.Services;
+
+import com.TRA.tra24Springboot.Models.*;
+import com.TRA.tra24Springboot.Repositories.OrderRepository;
+import com.TRA.tra24Springboot.Repositories.ProductDetailsRepository;
+import com.TRA.tra24Springboot.Repositories.ProductRepository;
+import com.TRA.tra24Springboot.Repositories.SupplierRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.UUID;
+
+@Service
+public class SupplierServices {
+    @Autowired
+    SupplierRepository supplierRepository; //reference to Supplier repo
+    @Autowired
+    ProductRepository productRepository; //reference to Product repo
+
+    @Autowired
+    ProductDetailsRepository productDetailsRepository; //reference to Product Details repo
+    @Autowired
+    OrderRepository orderRepository; //reference to Order repo
+
+    @Autowired
+    ProductServices productServices;
+
+    public Supplier add(Supplier supplier) {
+
+        ProductDetails productDetails = new ProductDetails();
+        productDetails.setName("Apple");
+        productDetails.setColor("Green");
+        productDetails.setSize("Small");
+        productDetails.setPrice(10d);
+        productDetails.setCountryOfOrigin("USA");
+        productDetails.setDescription("Apple Product");
+        productDetails.setCreatedDate(new Date());
+        productDetails = productDetailsRepository.save(productDetails);
+
+        Product product = new Product();
+        product.setProductDetails(productDetails);
+        product.setSku(UUID.randomUUID());
+        product.setCategory("Electronics");
+        product.setQuantity(1);
+        product.setIsActive(Boolean.TRUE);
+        product.setCreatedDate(new Date());
+        product = productRepository.save(product);
+
+
+        Order order = new Order();
+        order.setProductsOrdered(Arrays.asList(product)); //setting the products lists
+        order.setCategoryName("Electronics");
+        order.setCreatedDate(new Date());
+        order.setOrderDate(new Date());
+        order.setStatus(OrderStatus.IN_PROGRESS);
+        order.setPaymentStatus(PaymentStatus.PAID);
+        order.setPaymentType(PaymentType.BANK_TRANSFER);
+        order.setDueDate(new Date());
+        order = orderRepository.save(order);
+
+        ContactDetails contactDetails = new ContactDetails();
+        contactDetails.setPhoneNumber("12345678");
+        contactDetails.setPostalCode("123");
+
+        supplier.setCompanyName("Dell");
+        supplier.setOrders(Arrays.asList(order));
+        supplier.setCountry("USA");
+        //supplier.setContactDetails(contactDetails);
+        supplier.setMinimumOrderQuantity(2);
+        supplier.setCreatedDate(new Date());
+        supplier.setIsActive(Boolean.TRUE);
+
+        return supplierRepository.save(supplier);
+    }
+
+    public String updateMinimumOrderQuantity(Integer id, Integer quantity){
+        Supplier supplier = supplierRepository.getById(id);
+        supplier.setMinimumOrderQuantity(quantity);
+        supplier.setUpdatedDate(new Date());
+
+        supplierRepository.save(supplier);
+        return "updated successfully";
+    }
+
+    public String remove(Integer id){
+        Supplier supplier = supplierRepository.getById(id);
+        supplier.setIsActive(Boolean.FALSE);
+        System.out.println(supplier.toString());
+        supplierRepository.save(supplier);
+        return "Removed Successfully";
+    }
 }
