e2b27fc4a1b90f35ba6b6972baab2cda01b9cd39
AfnanSSa <iafnanz67@gmail.com>
2024-05-13 13:15:18 +0400

    Created Product Repository

--- src/main/java/com/TRA/tra24Springboot/Repositories/ProductRepository.java
+++ src/main/java/com/TRA/tra24Springboot/Repositories/ProductRepository.java
@@ -1,2 +1,8 @@
-package com.TRA.tra24Springboot.Repositories;public interface ProductRepository {
+package com.TRA.tra24Springboot.Repositories;
+
+import com.TRA.tra24Springboot.Models.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository <Product, Integer>{
+
 }
