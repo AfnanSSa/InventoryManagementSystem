ef54cd5c582b73fefb020670bde803d34f4c7c01
AfnanSSa <iafnanz67@gmail.com>
2024-05-15 12:13:23 +0400

    Created Inventory Services Class and added services methods

---
+++ src/main/java/com/TRA/tra24Springboot/Services/InventoryServices.java
@@ -0,0 +1,108 @@
+package com.TRA.tra24Springboot.Services;
+
+import com.TRA.tra24Springboot.DTO.InventoryDTO;
+import com.TRA.tra24Springboot.Models.*;
+import com.TRA.tra24Springboot.Repositories.InventoryRepository;
+import com.TRA.tra24Springboot.Repositories.OrderRepository;
+import com.TRA.tra24Springboot.Repositories.ProductRepository;
+import com.TRA.tra24Springboot.Repositories.SupplierRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class InventoryServices {
+
+    @Autowired
+    InventoryRepository inventoryRepository;
+    @Autowired
+    ProductRepository productRepository;
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Autowired
+    SupplierRepository supplierRepository;
+
+    InventoryDTO inventoryDTO;
+
+    //method for receiving new stock
+    public Inventory receiveNewStock(Inventory inventory){
+
+        ProductDetails productDetails = new ProductDetails();
+        productDetails.setName("Laptop");
+        productDetails.setColor("Black");
+        productDetails.setPrice(350d);
+        productDetails.setCountryOfOrigin("USA");
+        productDetails.setCreatedDate(new Date());
+
+        Product product = new Product();
+        product.setProductDetails(productDetails);
+        product.setSku(UUID.randomUUID());
+        product.setQuantity(100);
+        product.setIsActive(Boolean.TRUE);
+        product.setCreatedDate(new Date());
+
+        product = productRepository.save(product);
+
+        Order order = new Order();
+        order.setProductsOrdered(Arrays.asList(product)); //setting the products lists
+        order.setCategoryName("Electronics");
+        order.setCreatedDate(new Date());
+        order.setOrderDate(new Date());
+        order.setStatus(OrderStatus.IN_PROGRESS);
+        order.setPaymentStatus(PaymentStatus.PAID);
+        order.setPaymentType(PaymentType.BANK_TRANSFER);
+        order.setDueDate(new Date());
+        order = orderRepository.save(order);
+
+        Supplier supplier = new Supplier();
+        supplier.setCompanyName("Dell");
+        supplier.setOrders(Arrays.asList(order));
+        supplier.setCountry("USA");
+        //supplier.setContactDetails(contactDetails);
+        supplier.setMinimumOrderQuantity(2);
+        supplier.setCreatedDate(new Date());
+        supplier.setIsActive(Boolean.TRUE);
+        supplier = supplierRepository.save(supplier);
+
+        inventory.setProducts(Arrays.asList(product));
+        inventory.setLocation("Sohar");
+        inventory.setAdmin("Afnan");
+        inventory.setPhoneNumber("12345678");
+        inventory.setSupplier(Arrays.asList(supplier));
+        inventory.setOpeningHours("8 AM");
+        inventory.setClosingHours("8 PM");
+        inventory.setWorkers(Arrays.asList("Jack", "Andrew", "Sam"));
+        inventory.setCreatedDate(new Date());
+
+        return inventoryRepository.save(inventory);
+    }
+
+    //method to returns
+    public Inventory returns(Integer id) {
+        Inventory inventory = inventoryRepository.getById(id);
+
+        return inventory;
+    }
+
+    //method of write-offs
+    public Inventory writeOff(Integer id){
+        Inventory inventory = inventoryRepository.getById(id);
+        inventory.setIsActive(Boolean.FALSE);
+        inventory.setUpdatedDate(new Date());
+
+        return inventoryRepository.save(inventory);
+    }
+
+    //method for reporting inventory
+    public List<InventoryDTO> getInventory() {
+        List<Inventory> inventories = inventoryRepository.findAll();
+        return inventoryDTO.convertToDTOList(inventories);
+    }
+
+}
