9459e5f5655f373faa59b75d148bc2bee4b519ea
AfnanSSa <iafnanz67@gmail.com>
2024-05-26 12:13:51 +0400

    Queries of getting product by ID and country

--- src/main/java/com/TRA/tra24Springboot/Repositories/ProductRepository.java
+++ src/main/java/com/TRA/tra24Springboot/Repositories/ProductRepository.java
@@ -5,25 +5,23 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.Date;
 import java.util.List;
-import java.util.UUID;
 
 public interface ProductRepository extends JpaRepository <Product, Integer>{
 
     //Query to get product by ID
-    @Query("SELECT p FROM Product p WHERE p.id=:productID")
-    Product getProductByID(@Param("productID") Integer id);
+    @Query("SELECT p FROM Product p WHERE p.id =:productID")
+    Product getProductByID(@Param("productID") Integer productID);
 
     //Query to get product by name
     @Query("SELECT p FROM Product p WHERE p.productDetails.name =:product")
     List<Product> getProductByName (@Param("product") String product);
 
-/*
-    //Query to get product by country of origin
-    @Query ("SELECT p FROM Product p WHERE p.ProductDetails.countryOfOrigin=:country")
-    Product getProductByCountryOfOrigin(@Param("country") String countryOfOrigin);
 
+    //Query to get product by country of origin
+    @Query ("SELECT p FROM Product p WHERE p.productDetails.countryOfOrigin =:country")
+    List<Product> getProductByCountryOfOrigin(@Param("country") String countryOfOrigin);
+/*
     //Query to get product by expiry date
     @Query("SELECT p FROM Product p WHERE p.expiryDate=:expiryDate")
     Product getProductByExpiryDate(@Param("expiryDate") Date expiryDate);
