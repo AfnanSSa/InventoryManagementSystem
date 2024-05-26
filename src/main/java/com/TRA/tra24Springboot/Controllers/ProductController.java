e1b0c688238a0cfc757babdc3e3fa823137d5858
AfnanSSa <iafnanz67@gmail.com>
2024-05-26 12:32:50 +0400

    Added an API to get product by SKU

--- src/main/java/com/TRA/tra24Springboot/Controllers/ProductController.java
+++ src/main/java/com/TRA/tra24Springboot/Controllers/ProductController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/product")
@@ -59,6 +60,11 @@ public class ProductController {
     @GetMapping("getByColor")
     public List<Product> getProductByColor(@RequestParam String color){
         return productServices.getProductByColor(color);
+    }
+
+    @GetMapping("getBySKU")
+    public Product getProductByID(@RequestParam UUID sku){
+        return productServices.getProductBySKU(sku);
     }
 
 }
