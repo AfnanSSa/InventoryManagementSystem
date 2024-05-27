71f6007b0fd7d0e5db254487a5c6066737803e25
AfnanSSa <iafnanz67@gmail.com>
2024-05-27 12:24:23 +0400

    Query to get supplier by ID

--- src/main/java/com/TRA/tra24Springboot/Repositories/SupplierRepository.java
+++ src/main/java/com/TRA/tra24Springboot/Repositories/SupplierRepository.java
@@ -7,4 +7,9 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.RequestParam;
 
 public interface SupplierRepository extends JpaRepository <Supplier, Integer> {
+
+    //Query to get supplier by ID
+    @Query("SELECT s FROM Supplier s WHERE s.id =:supplierID")
+    Supplier getSupplierById(@Param("supplierID") Integer supplierID);
+
 }
