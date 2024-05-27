26bafb60cadf958b8a50da7fb020eb622a5defaf
AfnanSSa <iafnanz67@gmail.com>
2024-05-27 12:34:03 +0400

    Query to get supplier by company name

--- src/main/java/com/TRA/tra24Springboot/Repositories/SupplierRepository.java
+++ src/main/java/com/TRA/tra24Springboot/Repositories/SupplierRepository.java
@@ -6,10 +6,17 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 public interface SupplierRepository extends JpaRepository <Supplier, Integer> {
 
     //Query to get supplier by ID
     @Query("SELECT s FROM Supplier s WHERE s.id =:supplierID")
     Supplier getSupplierById(@Param("supplierID") Integer supplierID);
+
+    //Query to get supplier by company name
+    @Query("SELECT s FROM Supplier s WHERE s.companyName =:companyName")
+    List<Supplier> getSupplierByCompanyName(@Param("companyName") String companyName);
+
 
 }
