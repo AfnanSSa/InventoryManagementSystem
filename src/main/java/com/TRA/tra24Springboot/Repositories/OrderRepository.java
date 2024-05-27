4e56027c19147957d7fffb250c9e890fcb5b15f0
AfnanSSa <iafnanz67@gmail.com>
2024-05-27 11:59:46 +0400

    Query to get order by payment status

--- src/main/java/com/TRA/tra24Springboot/Repositories/OrderRepository.java
+++ src/main/java/com/TRA/tra24Springboot/Repositories/OrderRepository.java
@@ -2,6 +2,7 @@ package com.TRA.tra24Springboot.Repositories;
 
 import com.TRA.tra24Springboot.Models.Order;
 import com.TRA.tra24Springboot.Models.OrderStatus;
+import com.TRA.tra24Springboot.Models.PaymentStatus;
 import org.aspectj.weaver.ast.Or;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -22,4 +23,9 @@ public interface OrderRepository extends JpaRepository <Order, Integer> {
     //Query to get order by status
     @Query("SELECT o FROM Order o WHERE o.status =:status")
     List<Order> getOrderByStatus(@Param("status") OrderStatus status);
+
+    //Query to get order by payment status
+    @Query("SELECT o FROM Order o WHERE o.paymentStatus =:paymentStatus")
+    List<Order> getOrderByPaymentStatus(@Param("paymentStatus")PaymentStatus paymentStatus);
+
 }
