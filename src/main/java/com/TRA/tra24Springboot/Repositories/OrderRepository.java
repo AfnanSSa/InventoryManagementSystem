36f00cd8bc7ca95e3f069e9b42ffe516ee7a712b
AfnanSSa <iafnanz67@gmail.com>
2024-05-27 10:01:04 +0400

    Query to get order by ID

--- src/main/java/com/TRA/tra24Springboot/Repositories/OrderRepository.java
+++ src/main/java/com/TRA/tra24Springboot/Repositories/OrderRepository.java
@@ -2,6 +2,13 @@ package com.TRA.tra24Springboot.Repositories;
 
 import com.TRA.tra24Springboot.Models.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface OrderRepository extends JpaRepository <Order, Integer> {
+
+    //Query to get order by ID 
+    @Query("SELECT o FROM Order o WHERE o.id =:orderID")
+    Order getOrderById(@Param("orderID") Integer id);
+
 }
