a9c37268c01eb5f9a3543717845b3ac5e4bd56e8
AfnanSSa <iafnanz67@gmail.com>
2024-05-28 10:01:38 +0400

    Query to get inventory by ID

--- src/main/java/com/TRA/tra24Springboot/Repositories/InventoryRepository.java
+++ src/main/java/com/TRA/tra24Springboot/Repositories/InventoryRepository.java
@@ -2,6 +2,13 @@ package com.TRA.tra24Springboot.Repositories;
 
 import com.TRA.tra24Springboot.Models.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface InventoryRepository extends JpaRepository <Inventory, Integer> {
+
+    //Query to get inventory by ID
+    @Query("SELECT i FROM Inventory i WHERE i.id =:inventoryID")
+    Inventory getInventoryById(@Param("inventoryID") Integer inventoryID);
+
 }
