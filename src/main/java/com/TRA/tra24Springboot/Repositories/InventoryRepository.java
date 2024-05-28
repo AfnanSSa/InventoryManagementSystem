12733baa3775ee8e90422a38a7139026fe96efcc
AfnanSSa <iafnanz67@gmail.com>
2024-05-28 11:21:30 +0400

    Query to get inventory by availability

--- src/main/java/com/TRA/tra24Springboot/Repositories/InventoryRepository.java
+++ src/main/java/com/TRA/tra24Springboot/Repositories/InventoryRepository.java
@@ -5,10 +5,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface InventoryRepository extends JpaRepository <Inventory, Integer> {
 
     //Query to get inventory by ID
     @Query("SELECT i FROM Inventory i WHERE i.id =:inventoryID")
     Inventory getInventoryById(@Param("inventoryID") Integer inventoryID);
+
+    //Query to git inventory by availability
+    @Query("SELECT i FROM Inventory i WHERE i.isActive =:isActive")
+    List<Inventory> getInventoryByAvailability(@Param("isActive") Boolean isActive);
 
 }
