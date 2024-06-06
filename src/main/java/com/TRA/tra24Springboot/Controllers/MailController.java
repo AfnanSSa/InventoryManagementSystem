f9e071405d89df59c48bec6b0632f1895d560f95
AfnanSSa <iafnanz67@gmail.com>
2024-06-06 09:27:48 +0400

    Added API to send email

--- src/main/java/com/TRA/tra24Springboot/Controllers/MailController.java
+++ src/main/java/com/TRA/tra24Springboot/Controllers/MailController.java
@@ -1,2 +1,27 @@
-package com.TRA.tra24Springboot.Controllers;public class MailController {
+package com.TRA.tra24Springboot.Controllers;
+
+import com.TRA.tra24Springboot.Services.MailingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/mail")
+public class MailController {
+    @Autowired
+    MailingService mailingService;
+    @PostMapping("/message")
+    public String sendMail() {
+
+        return mailingService.sendSimpleMail();
+    }
+
+
+    @GetMapping("/sendWithAttachment")
+    public String sendMailWithAttachment() {
+
+        return mailingService.sendMailWithAttachment();
+    }
 }
