b10dfb2a75b114eee07c75d81f560c3de971f5dc
AfnanSSa <iafnanz67@gmail.com>
2024-07-02 11:55:17 +0400

    Created Invoice repository and added queries

---
+++ src/main/java/com/TRA/tra24Springboot/Repositories/InvoiceRepository.java
@@ -0,0 +1,20 @@
+package com.TRA.tra24Springboot.Repositories;
+
+import com.TRA.tra24Springboot.Models.Invoice;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Date;
+import java.util.List;
+
+public interface InvoiceRepository extends JpaRepository<Invoice, Integer> {
+    @Query("SELECT v FROM Invoice v WHERE v.id =:id")
+    public Invoice getInvoiceById(@Param("id") Integer id);
+
+    @Query("SELECT v FROM Invoice v WHERE v.createdDate =:createdDate")
+    public List<Invoice> getInvoiceByCreatedDate(@Param("createdDate")Date createdDate);
+
+    @Query("SELECT v FROM Invoice v WHERE v.dueDate =:dueDate")
+    public List<Invoice> getInvoiceByDueDate(@Param("dueDate") Date dueDate);
+}
