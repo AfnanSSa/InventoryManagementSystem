7f0375b6651e94b760226725bce5701456bc1b2e
AfnanSSa <iafnanz67@gmail.com>
2024-07-04 13:36:46 +0400

    Added API to send a monthly invoices report on Slack channel

--- src/main/java/com/TRA/tra24Springboot/Controllers/InvoiceController.java
+++ src/main/java/com/TRA/tra24Springboot/Controllers/InvoiceController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -57,7 +59,7 @@ public class InvoiceController {
         }
     }
 
-    @Scheduled(cron = "0 0 9 * * 0") //runs every Sunday 
+    @Scheduled(cron = "0 0 9 * * 0") //runs every Sunday
     @PostMapping("weeklyReport")
     public void weeklyInvoiceReport(){
         Date today = new Date();
@@ -69,6 +71,38 @@ public class InvoiceController {
 
         StringBuilder report = new StringBuilder();
         report.append("Weekly Summary Report:\n")
+                .append("Invoices Created:\n");
+        appendInvoicesToReport(report, createdInvoices);
+        report.append("\nInvoices Paid:\n");
+        appendInvoicesToReport(report, paidInvoices);
+        report.append("\nOverdue Invoices:\n");
+        appendInvoicesToReport(report, overdueInvoices);
+
+        slackService.sendMessage("afnan", report.toString());
+    }
+
+    @Scheduled(cron = "0 0 9 1 * ?") //runs on the first day of every month at 9:00 AM
+    @PostMapping("monthlyReport")
+    public void monthlyInvoiceReport(){
+
+        //calculating start and end dates for the current month
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.DAY_OF_MONTH, 1); //setting to the first day of the month
+        Date startDate = calendar.getTime();
+
+        calendar.add(Calendar.MONTH, 1); //moving to the next month
+        calendar.add(Calendar.DAY_OF_MONTH, -1); //setting to the last day of the current month
+        Date endDate = calendar.getTime();
+
+        List<Invoice> createdInvoices = invoiceServices.getInvoicesCreatedBetween(startDate, endDate);
+        List<Invoice> paidInvoices = invoiceServices.getPaidInvoicesBetween(startDate, endDate);
+        List<Invoice> overdueInvoices = invoiceServices.getOverDueInvoices();
+
+        StringBuilder report = new StringBuilder();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MMMM yyyy");
+        report.append("Monthly Invoices Report - ")
+                .append(dateFormat.format(startDate))
+                .append(":\n")
                 .append("Invoices Created:\n");
         appendInvoicesToReport(report, createdInvoices);
         report.append("\nInvoices Paid:\n");
