14f94edf24dc64e2affc1fa394fac26483688599
AfnanSSa <iafnanz67@gmail.com>
2024-07-04 13:27:04 +0400

    Add query methods to get invoices by date ranges

--- src/main/java/com/TRA/tra24Springboot/Repositories/InvoiceRepository.java
+++ src/main/java/com/TRA/tra24Springboot/Repositories/InvoiceRepository.java
@@ -24,4 +24,12 @@ public interface InvoiceRepository extends JpaRepository<Invoice, Integer> {
 
     @Query("SELECT v FROM Invoice v WHERE v.dueDate < :today")
     public List<Invoice> getOverdueInvoices(@Param("today") Date today);
+
+    @Query("SELECT v FROM Invoice v WHERE v.createdDate BETWEEN :startDate AND :endDate")
+    public List<Invoice> getInvoicesCreatedBetween(@Param("startDate") Date startDate,
+                                                   @Param("endDate") Date endDate);
+
+    @Query("SELECT v FROM Invoice v WHERE v.paymentDate BETWEEN :startDate AND :endDate")
+    public List<Invoice> getPaidInvoicesBetween(@Param("startDate") Date startDate,
+                                                 @Param("endDate") Date endDate);
 }
