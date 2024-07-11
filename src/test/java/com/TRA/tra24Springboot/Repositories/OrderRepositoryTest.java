df0c6ada13801c8e6859de2238961e24f539a433
AfnanSSa <iafnanz67@gmail.com>
2024-07-11 12:14:22 +0400

    Create OrderRepositoryTest.java

---
+++ src/test/java/com/TRA/tra24Springboot/Repositories/OrderRepositoryTest.java
@@ -0,0 +1,28 @@
+package com.TRA.tra24Springboot.Repositories;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderRepositoryTest {
+
+    @Test
+    void getOrderById() {
+    }
+
+    @Test
+    void getOrderByCategoryName() {
+    }
+
+    @Test
+    void getOrderByStatus() {
+    }
+
+    @Test
+    void getOrderByPaymentStatus() {
+    }
+
+    @Test
+    void getOrderByPaymentType() {
+    }
+}
