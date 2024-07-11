44ab868dfeb88fae81eb53cfb62c6e0ba09e25ce
AfnanSSa <iafnanz67@gmail.com>
2024-07-11 11:12:32 +0400

    Add a Builder

--- src/main/java/com/TRA/tra24Springboot/Models/Product.java
+++ src/main/java/com/TRA/tra24Springboot/Models/Product.java
@@ -2,12 +2,18 @@ package com.TRA.tra24Springboot.Models;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Data
 @Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product extends BaseEntity {
 
     Integer quantity;
