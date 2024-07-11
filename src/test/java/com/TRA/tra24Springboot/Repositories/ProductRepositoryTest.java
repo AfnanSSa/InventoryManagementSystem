e7a0fb72ded65015fc86609098f9a7cf7bf3f13a
AfnanSSa <iafnanz67@gmail.com>
2024-07-11 11:15:07 +0400

    Created Unit Test for getProductByName

---
+++ src/test/java/com/TRA/tra24Springboot/Repositories/ProductRepositoryTest.java
@@ -0,0 +1,91 @@
+package com.TRA.tra24Springboot.Repositories;
+
+import com.TRA.tra24Springboot.Models.Product;
+import com.TRA.tra24Springboot.Models.ProductDetails;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@DataJpaTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+
+class ProductRepositoryTest {
+
+    @Autowired
+    private ProductRepository productRepository;
+    @Autowired
+    private ProductDetailsRepository productDetailsRepository;
+    @BeforeEach
+    void setupProduct(){
+        ProductDetails productDetails = ProductDetails.builder()
+                .name("Screen")
+                .color("Black")
+                .countryOfOrigin("Japan")
+                .price(200.0)
+                .size("24 inches")
+                .build();
+
+        productDetailsRepository.save(productDetails);
+
+        Product product = Product.builder()
+                .productDetails(productDetails)
+                .category("Electronics")
+                .quantity(50)
+                .build();
+
+        productRepository.save(product);
+    }
+/*    @Test
+    void testGetProductByID() {
+        Product product = productRepository.getProductByID();
+    }*/
+
+    @Test
+    void getProductByName() {
+        List<Product> product = productRepository.getProductByName("Screen");
+        assertThat(product).isNotNull();
+        assertThat(product.size()).isEqualTo(1);
+    }
+
+    @Test
+    void getProductByCountryOfOrigin() {
+    }
+
+   /* @Test
+    void getProductBySize() {
+    }
+
+    @Test
+    void getProductByColor() {
+    }
+
+    @Test
+    void getProductBySKU() {
+    }
+
+    @Test
+    void getProductByCategory() {
+    }
+
+    @Test
+    void getProductByPrice() {
+    }
+
+    @Test
+    void getProductByAvailability() {
+    }
+
+    @Test
+    void getProductByQuantity() {
+    }*/
+}
