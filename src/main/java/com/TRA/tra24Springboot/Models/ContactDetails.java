267049f9a2730d587422afc3a3a1b5a1fb78764d
AfnanSSa <iafnanz67@gmail.com>
2024-07-14 10:14:39 +0400

    Added Builder

--- src/main/java/com/TRA/tra24Springboot/Models/ContactDetails.java
+++ src/main/java/com/TRA/tra24Springboot/Models/ContactDetails.java
@@ -1,13 +1,18 @@
 package com.TRA.tra24Springboot.Models;
 
 import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Cascade;
 import org.hibernate.annotations.CascadeType;
 
 @Data
 @Entity
-
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ContactDetails extends BaseEntity {
 
     String email;
