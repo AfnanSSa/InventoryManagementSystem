13cf53af6e6d52f166224b3aa694cddf741c501c
AfnanSSa <iafnanz67@gmail.com>
2024-07-14 10:14:56 +0400

    Added Builder

--- src/main/java/com/TRA/tra24Springboot/Models/Supplier.java
+++ src/main/java/com/TRA/tra24Springboot/Models/Supplier.java
@@ -4,13 +4,19 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 import java.util.List;
 
 @Data
 @Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Supplier extends BaseEntity{
 
     String companyName;
