ff79b1edeae20dca96623f3ef4c37b70e52d25e3
AfnanSSa <iafnanz67@gmail.com>
2024-07-14 09:39:53 +0400

    Create SupplierRepositoryTest.java

---
+++ src/test/java/com/TRA/tra24Springboot/Repositories/SupplierRepositoryTest.java
@@ -0,0 +1,32 @@
+package com.TRA.tra24Springboot.Repositories;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SupplierRepositoryTest {
+
+    @Test
+    void getSupplierById() {
+    }
+
+    @Test
+    void getSupplierByCompanyName() {
+    }
+
+    @Test
+    void getSupplierByCountry() {
+    }
+
+    @Test
+    void getSupplierByMinimumOrderQty() {
+    }
+
+    @Test
+    void getSupplierByShippingMethod() {
+    }
+
+    @Test
+    void getSupplierByPaymentMethod() {
+    }
+}
