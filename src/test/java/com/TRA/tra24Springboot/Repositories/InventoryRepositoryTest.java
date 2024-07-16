1d9efcd627a7822574fefcf77c6bdcd71c03675f
AfnanSSa <iafnanz67@gmail.com>
2024-07-16 10:00:42 +0400

    removed unused imports

--- src/test/java/com/TRA/tra24Springboot/Repositories/InventoryRepositoryTest.java
+++ src/test/java/com/TRA/tra24Springboot/Repositories/InventoryRepositoryTest.java
@@ -15,7 +15,6 @@ import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
-import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
