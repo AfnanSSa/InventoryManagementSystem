7ad6754009e560a74007efd6dc517475292046fe
AfnanSSa <iafnanz67@gmail.com>
2024-07-16 10:14:21 +0400

    Added Builder

--- src/main/java/com/TRA/tra24Springboot/Models/Invoice.java
+++ src/main/java/com/TRA/tra24Springboot/Models/Invoice.java
@@ -2,13 +2,19 @@ package com.TRA.tra24Springboot.Models;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 import java.util.List;
 
 @Data
 @Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Invoice extends BaseEntity{
     @OneToMany
     List<Product> products;
