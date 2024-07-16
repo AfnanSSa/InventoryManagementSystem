4f3953880e562658e118182c123764e9a438b300
AfnanSSa <iafnanz67@gmail.com>
2024-07-16 09:45:50 +0400

    Added Builder

--- src/main/java/com/TRA/tra24Springboot/Models/Inventory.java
+++ src/main/java/com/TRA/tra24Springboot/Models/Inventory.java
@@ -3,13 +3,18 @@ package com.TRA.tra24Springboot.Models;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @Entity
-
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Inventory extends BaseEntity {
 
     @OneToMany
